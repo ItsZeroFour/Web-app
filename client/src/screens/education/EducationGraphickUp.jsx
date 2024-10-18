db595c1110cb8f04f018067789ec545fa4e1b45c
itsZeroFour <itsZeroFour@gmail.com>
2024-10-18 09:51:27 +0300

    fix

--- client/src/screens/education/EducationGraphickUp.jsx
+++ client/src/screens/education/EducationGraphickUp.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./style.module.scss";
 import person from "../../assets/images/after-chat-4.png";
 import { useTranslation } from "react-i18next";
 import graphick from "../../assets/graphicks/from_top_to_bottom.json";
 import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
+import { motion } from "framer-motion";
 
 const EducationGraphickUp = () => {
   const { t } = useTranslation();
+
+  const [text, setText] = useState("");
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const changeText = setTimeout(() => {
+      setText(t("educationGraphickText"));
+    }, 4500);
+
+    const showButton = setTimeout(() => {
+      setShowButton(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(changeText);
+      clearTimeout(showButton);
+    };
+  }, []);
 
   return (
     <section className={style.education}>
@@ -20,18 +39,18 @@ const EducationGraphickUp = () => {
 
             <div>
               <h3>{t("education1Name")}</h3>
-              <p>{t("education7Text")}</p>
+              <p>{text ? text : t("education7Text")}</p>
             </div>
           </div>
 
           <div className={style.educatio7__balance}>
             <div className={style.educatio7__balance__demo}>
               <p>Your demo balance</p>
-              <h1>10000₹</h1>
+              <h1>9900₹</h1>
             </div>
 
             <div className={style.educatio7__balance__currency}>
-              <p>EUR/USD</p>
+              <p>USD/EUR</p>
             </div>
           </div>
 
@@ -42,8 +61,17 @@ const EducationGraphickUp = () => {
             style={{ width: "90%", height: 350 }}
           />
 
-          <div className={style.educatio_graphick__buttons}>
-            <Link to="#">+150 ₹</Link>
+          <div style={{ height: 60, width: "100%" }}>
+            {showButton && (
+              <motion.div
+                className={style.educatio_graphick__buttons}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Link to="#">Ok</Link>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
